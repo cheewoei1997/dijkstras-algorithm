c5fa8cbbc2d4acba8f889496a84033edd86c0ae7
Phuah Chee Woei <cheewoei1997@gmail.com>
2019-01-12 14:57:05 +0800

    Add automated graph drawing

--- dijkstras-algov1.py
+++ dijkstras-algov1.py
@@ -2,7 +2,16 @@
 # ------------------------------------------------------------------------------
 from collections import namedtuple, deque
 from pprint import pprint as pp
-import networkxApi as networkGraph
+import matplotlib.pyplot as plt
+import numpy as np
+
+# https://networkx.github.io/documentation/networkx-1.10/tutorial/tutorial.html#drawing-graphs
+import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+
+# https://stackoverflow.com/questions/40632486/dot-exe-not-found-in-path-pydot-on-python-windows-7
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
  
 # ------------------------------------------------------------------------------
@@ -10,7 +19,7 @@ import networkxApi as networkGraph
 start_node = 'a'
 dest_node = 'h'
 
-# Specify the graph using the following format: (node1, node2, weight)
+# Specify the graph using the following format: (startNode, endNode, weight)
 graphItem = [('a', 'b', 9),  ('a', 'f', 14),  ('a', 'g', 14), ('b', 'c', 23),
                ('c', 'f', 18), ('c', 'd', 6), ('c', 'h', 19), ('c', 'e', 2),
                ('d', 'e', 11), ('d', 'h', 6), ('e', 'f', 30), ('e', 'g', 20),
@@ -36,7 +45,7 @@ class Graph():
         for start, end, cost in self.edges:
             neighbours[start].add((end, cost))
         print('NEIGHBOURS')
-        pp(neighbours)
+        # pp(neighbours)
  
         while q:
             u = min(q, key=lambda vertex: dist[vertex])
@@ -49,7 +58,7 @@ class Graph():
                     dist[v] = alt
                     previous[v] = u
         print('\nSMALLEST VALUE')
-        pp(previous)
+        # pp(previous)
         s, u = deque(), dest
         while previous[u]:
             s.appendleft(u)
@@ -58,30 +67,51 @@ class Graph():
         return s
 
 
-def drawGraph():
-    networkGraph.addEdge(1, 2, 9)
-    networkGraph.addEdge(1, 6, 14)
-    networkGraph.addEdge(1, 7, 14)
-    networkGraph.addEdge(2, 3, 23)
-    networkGraph.addEdge(3, 4, 6)
-    networkGraph.addEdge(3, 5, 2)
-    networkGraph.addEdge(3, 6, 18)
-    networkGraph.addEdge(3, 8, 19)
-    networkGraph.addEdge(4, 5, 11)
-    networkGraph.addEdge(4, 8, 6)
-    networkGraph.addEdge(5, 6, 30)
-    networkGraph.addEdge(5, 7, 20)
-    networkGraph.addEdge(5, 8, 16)
-    networkGraph.addEdge(6, 7, 5)
-    networkGraph.addEdge(7, 8, 44)
-    networkGraph.drawGraph()
-    networkGraph.showGraph()
+def drawGraph(totalNodes, startNode, endNode, weight):
+    # Creating a graph
+    # Create an empty graph with no nodes and no edges.
+    G = nx.Graph()
+    
+    # Add edges or nodes into the graph
+    for i in range(totalNodes):
+        G.add_edge(startNode[i], endNode[i], weight=weight[i])
+
+    # Specify positioning of the graph using a dictionary
+    # pos = nx.spring_layout(G, scale=2, k=np.sqrt(len(G.nodes)))
+    # https://networkx.github.io/documentation/stable/reference/drawing.html
+    pos = nx.nx_pydot.graphviz_layout(G, prog='neato')
+
+    # Keep nodes and edge labels in a dictionary
+    edge_labels = dict([((u, v,), d['weight']) for u, v, d in G.edges(data=True)])
+
+    nx.draw(G, pos, edge_color='black', width=1, linewidths=1, node_size=500,
+        node_color='pink', labels={node:node for node in G.nodes()})
+
+    # Draw out the edge labels
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels,
+        font_color='black')
+
+    showGraph()
+
+
+# Show the drawn graph
+def showGraph():
+    plt.axis('off')
+    plt.show()
 
 
 def main():
     graph = Graph(graphItem)
-    pp(graph.dijkstra(start_node, dest_node))
-    drawGraph()
+    # pp(graph.dijkstra(start_node, dest_node))
+    path = graph.dijkstra(start_node, dest_node)
+
+    totalNodes = len(graphItem)
+    startNode = [item[0] for item in graphItem]
+    endNode = [item[1] for item in graphItem]
+    weight = [item[2] for item in graphItem]
+
+    drawGraph(totalNodes, startNode, endNode, weight)
+
 
 if __name__ == "__main__":
     main()
